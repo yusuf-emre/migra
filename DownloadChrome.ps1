5598531f4598054921533061947c9af5c7ecc1d3
yusuf-emre <velioglu.ye@gmail.com>
2023-12-04 23:06:20 +0100

    add chrome download script to pipeline

---
+++ DownloadChrome.ps1
@@ -0,0 +1,7 @@
+# Download ChromeDriver
+Invoke-WebRequest -Uri https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/119.0.6045.105/win32/chromedriver-win32.zip -OutFile chromedriver-win32.zip
+Expand-Archive -Path chromedriver-win32.zip -DestinationPath .
+
+# Download Chrome Binary
+Invoke-WebRequest -Uri https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/119.0.6045.105/win32/chrome-win32.zip -OutFile chrome-win32.zip
+Expand-Archive -Path chrome-win32.zip -DestinationPath .
--- Migra.cs
+++ Migra.cs
@@ -22,8 +22,8 @@ namespace Migra
         [Function("Migra")]
         public void Run([TimerTrigger("0 0 * * * *")] TimerInfo myTimer)
         {
-            string driverPath = Path.Combine(Environment.CurrentDirectory, "Drivers", "chromedriver.exe");
-            string binaryPath = Path.Combine(Environment.CurrentDirectory, "Drivers", "chrome.exe");
+            string driverPath = Path.Combine(Environment.CurrentDirectory, "chromedriver.exe");
+            string binaryPath = Path.Combine(Environment.CurrentDirectory, "chrome.exe");
 
             ChromeOptions options = new ChromeOptions();
             options.BinaryLocation = binaryPath;
